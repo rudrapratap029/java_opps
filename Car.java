c7e74720675fb18a4e3b8d3bdc497e1ec5abdac0
rudrapratap029 <rudrapratap112005@gmail.com>
2026-09-20 14:50:19 +0530

    Add Car class

---
+++ Car.java
@@ -0,0 +1,13 @@
+package java_opps;
+
+class Car {
+
+    public void run() {
+        System.out.println("Car is moving at its highest speed");
+    }
+
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.run();
+    }
+}
