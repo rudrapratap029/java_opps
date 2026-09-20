fbb64fd09105ebd3b98f8d4d13547c2eed7c7199
rudrapratap029 <rudrapratap112005@gmail.com>
2026-09-20 16:26:32 +0530

    single inheritance

---
+++ Vehicle.java
@@ -0,0 +1,18 @@
+package java_opps;
+
+//  Single inhertance ;
+
+class Vehicle{
+ void prices(){
+  System.out.println("Fortuner has a more price than vehicle");
+ }
+}
+class Carnew extends Vehicle{
+   public static void main(String[] args){
+    Carnew c1 = new Carnew();
+     Carnew c2 = new Carnew();
+     c2.prices();
+    c1.prices();
+    }
+}
+
