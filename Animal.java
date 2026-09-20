ba79905773cc6082ac6e5541722b801c3f4d9bb1
rudrapratap029 <rudrapratap112005@gmail.com>
2026-09-20 14:55:53 +0530

    Add animal class

---
+++ Animal.java
@@ -0,0 +1,16 @@
+package java_opps;
+
+public class Animal {
+
+  public void run(){
+    System.out.println("Animal can run faster than human");
+  }
+
+  public static void main(String [] args){
+  Animal a1 = new Animal();
+  a1.run();
+  Animal a2 = new Animal();
+  a2.run();
+  }
+  
+}
