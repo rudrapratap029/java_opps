bf7f975cdb1b0d5eab4cb47c756da52bf49078df
rudrapratap029 <rudrapratap112005@gmail.com>
2026-09-20 16:37:31 +0530

    multilevel inheritance

---
+++ A.java
@@ -0,0 +1,29 @@
+package java_opps;
+
+public class A {
+
+  void show(){
+    System.out.println(" shows A method");
+  }
+  
+}
+
+class B extends A{
+  public void  run(){
+      System.out.println("Hey");
+    }
+
+ 
+   
+  
+
+}
+
+class C extends B{
+    public static void main(String[] args){
+      C c = new C();
+      c.show();
+      c.run();
+
+  }
+}
--- Vehicle.java
+++ Vehicle.java
@@ -16,3 +16,5 @@ class Carnew extends Vehicle{
     }
 }
 
+
+
