73febda61b4085e9a756be3470477ef55acfa0b9
rudrapratap029 <rudrapratap112005@gmail.com>
2026-09-21 16:50:10 +0530

    abstraction

---
+++ Abstraction/Vehicle.java
@@ -0,0 +1,30 @@
+package java_opps.Abstraction;
+
+ abstract class Vehicle {
+
+  abstract void start();
+
+ }
+
+  class Car extends Vehicle{
+    void start(){
+      System.out.println("Car is start with key");
+    }
+
+  }
+
+  class Scooter extends Vehicle{
+    void start(){
+      System.out.println("scooter start with Kick");
+    }
+    public static void main(String[] args){
+      Car c = new Car();
+      
+      c.start();
+      Scooter s = new Scooter();
+      s.start();
+    }
+
+  }
+  
+
