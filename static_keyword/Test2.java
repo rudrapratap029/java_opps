56e8a0bf798529f9cddf3f5487419812e5310bdd
rudrapratap029 <rudrapratap112005@gmail.com>
2026-09-23 16:05:39 +0530

    static variable

---
+++ static_keyword/Test2.java
@@ -0,0 +1,15 @@
+package java_opps.static_keyword;
+
+
+// we can also excess static variable by the name of the class 
+public class Test2 {
+
+  static int b = 9;
+  
+}
+
+class Demo{
+  public static void main (String [] args){
+    System.out.print(Test2.b);
+  }
+}
