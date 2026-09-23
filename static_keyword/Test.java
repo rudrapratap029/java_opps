a46a74f27ad57782130d2f17933ffc8c84e01ad6
rudrapratap029 <rudrapratap112005@gmail.com>
2026-09-23 16:01:25 +0530

    static variable

---
+++ static_keyword/Test.java
@@ -0,0 +1,16 @@
+package java_opps.static_keyword;
+
+public class Test {
+
+ static int i = 54;
+  // void show(){
+  //  static int i= 5;    // here this line gives error bc we can not declare static variable in a local variable
+  //   System.out.println(i);
+
+  // }
+
+  public static void main(String[]args){
+    System.out.println(i);
+  }
+  
+}
